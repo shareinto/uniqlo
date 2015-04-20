f632453580ba9b305e7beb5ce33eae9a127b274c
unknown <shareinto>
2015-04-21 03:19:23 +0800

    new file:   packages/DotNetOpenAuth.AspNet.4.1.4.12333/DotNetOpenAuth.AspNet.4.1.4.12333.nupkg

---
+++ uniqlo/App_Code/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace uniqlo
+{
+    public static class Config
+    {
+        public static string RES_HOST = ConfigurationManager.AppSettings["res"];
+    }
+}
---
+++ uniqlo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="uniqlo.MvcApplication" Language="C#" %>
